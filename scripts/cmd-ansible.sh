e4b9107ec504e0824a541b746e0c660b3a4419a8
Axel Stage <dataengineer24@proton.me>
2026-07-28 18:50:37 +0200

    dev: build install postgresql playbook

--- scripts/cmd-ansible.sh
+++ scripts/cmd-ansible.sh
@@ -2,41 +2,13 @@
 # Ansible #
 ###########
 
-# install
-sudo apt update
-sudo apt install -y ansible
+# activate ansible
+##################
+cd ansible && pwd
+source .venv/bin/activate
 ansible --version
+clear
 
-
-# Setting Up SSH Key Authentication
-ssh-keygen -t ed25519 -C "ansible-control-node" -f ~/.ssh/ansible_key
-# Set correct permissions on the private key
-chmod 600 ~/.ssh/ansible_key
-
-# Copy SSH key to a single managed node
-# Replace 'username' with your actual username and 'server_ip' with the target IP
-ssh-copy-id -i ~/.ssh/ansible_key.pub username@server_ip
-
-
-cat << EOF >> ~/.ssh/config
-
-# Ansible managed servers configuration
-Host ansible-*
-    User ubuntu
-    IdentityFile ~/.ssh/ansible_key
-    StrictHostKeyChecking no
-    UserKnownHostsFile /dev/null
-
-# Specific server aliases
-Host db-server-1
-    HostName 192.168.1.11
-    User ubuntu
-    IdentityFile ~/.ssh/ansible_key
-EOF
-
-
-# test SSH Connection
-ssh ubuntu@3.70.47.150 -i ~/projects/aws_iac_pipeline/ansible/keys/ansible-key.pem "hostname && uptime"
 
 
 
@@ -44,23 +16,30 @@ ssh ubuntu@3.70.47.150 -i ~/projects/aws_iac_pipeline/ansible/keys/ansible-key.p
 # inventory
 ###########
 # List all hosts in the inventory
-ansible-inventory -i ansible/inventory/hosts.yml --list
+ansible-inventory -i inventories/hosts.yml --list
 
 # Display inventory in graph format
-ansible-inventory -i ansible/inventory/hosts.yml --graph
+ansible-inventory -i inventories/hosts.yml --graph
 
 # List hosts in a specific group
-ansible-inventory -i ansible/inventory/hosts.yml --graph db-servers
+ansible-inventory -i inventories/hosts.yml --graph dbserver
 
 
 # Basic syntax: ansible <pattern> -i <inventory> -m <module> -a "<arguments>"
 
 # Test connectivity to all hosts
-ansible all -i ansible/inventory/hosts.yml -m ping
-ansible dbserver -i ansible/inventory/hosts.yml -m ping
+ansible all -i inventories/hosts.yml -m ping
+ansible dbserver -i inventories/hosts.yml -m ping
 
 # Execute a shell command on all hosts
-ansible all -i ansible/inventory/hosts.yml -m shell -a "uptime"
+ansible all -i inventories/hosts.yml -m shell -a "uptime"
 
 # Check disk space on database servers
-ansible db-servers -i inventory/hosts.yml -m shell -a "df -h"
+ansible dbserver -i inventories/hosts.yml -m shell -a "df -h"
+
+
+
+# playbooks
+###########
+# Install PostgreSQL on all database servers
+ansible-playbook playbooks/install-postgresql.yml -i inventories/hosts.yml dbserver
