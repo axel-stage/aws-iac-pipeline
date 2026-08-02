ce7d09530bb8c39429168a9748d7f7ad071bbb9a
Axel Stage <dataengineer24@proton.me>
2026-08-02 08:29:34 +0200

    fix: issue on aws_instance, due to retired security groups argument

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,7 +9,7 @@ resource "aws_iam_instance_profile" "dbserver" {
 resource "aws_instance" "dbserver" {
   ami                         = data.aws_ami.ubuntu_noble_2404.id
   subnet_id                   = aws_subnet.public_az_a.id
-  security_groups             = [aws_security_group.dbserver.id]
+  vpc_security_group_ids      = [aws_security_group.dbserver.id]
   iam_instance_profile        = aws_iam_instance_profile.dbserver.id
   key_name                    = aws_key_pair.ansible.key_name
   instance_type               = var.ec2_instance_type
@@ -20,6 +20,10 @@ resource "aws_instance" "dbserver" {
     volume_size           = var.ec2_instance_volume_size
     volume_type           = "gp3"
     delete_on_termination = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
   }
 
   tags = {
